5da13bc71134313d5c359f8ff876e9294b1f74cc
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-10-16 20:43:30 -0600

    Add SQL script to calculate user statistics including total, min, max, avg followers, and rounded average

---
+++ seccion_3/lec_4/max_min_count_round_avg.sql
@@ -0,0 +1,9 @@
+select
+	COUNT(*) as total_users,
+    	MIN( followers ) as min_followers,
+	MAX( followers ) as max_followers,
+	AVG( followers ) as avg_followers,
+	ROUND( AVG(followers) ) as avg_followers_round,
+	SUM(followers) / COUNT(*) as avg_followers_manual
+from
+    	users;
