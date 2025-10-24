7e13d3603ddf503e3e1da9c398e7dde2fbc4df65
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-10-23 22:38:25 -0600

    Refactor SQL query for consistent indentation in max_min_count_round_avg.sql

--- seccion_3/lec_4/max_min_count_round_avg.sql
+++ seccion_3/lec_4/max_min_count_round_avg.sql
@@ -1,6 +1,6 @@
 select
 	COUNT(*) as total_users,
-    	MIN( followers ) as min_followers,
+    MIN( followers ) as min_followers,
 	MAX( followers ) as max_followers,
 	AVG( followers ) as avg_followers,
 	ROUND( AVG(followers) ) as avg_followers_round,
