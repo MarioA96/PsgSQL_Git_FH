c93b7f55fe5c620c7cff0971ac76dded5dd83a57
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-12-04 19:38:44 -0600

    Add SQL script to delete AFG entry and select related data from country, city, and countrylanguage tables

---
+++ seccion_4/lec_8/on_cascade.sql
@@ -0,0 +1,7 @@
+Delete From country where code = 'AFG';
+Select * From country where code = 'AFG';
+Select * From city Where countrycode = 'AFG';
+Select * From countrylanguage Where countrycode = 'AFG';
+
+ALTER TABLE your_child_table_name
+DROP CONSTRAINT foreign_key_constraint_name;
